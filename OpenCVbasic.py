641eeee1167bb1e50221caf4f14ca0a7f3945e5a
jpfeijo <j.feijo@edu.pucrs.br>
2022-12-05 10:55:05 -0300

    curso openCV

--- OpenCVbasic.py
+++ OpenCVbasic.py
@@ -10,7 +10,29 @@ cv.imshow('Dog', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-#blur
+# Blur
+blur = cv.GaussianBlur(img, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# Edge cascade (Bordas) -> Quanto menos blur, mais bordas serão detectadas (mais detalhes)
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+# Dilating the image 
+dilated = cv.dilate(canny, (5,5), iterations=1)
+cv.imshow('Dilated', dilated)
+
+# Eroding
+eroded = cv.erode(dilated, (5,5), iterations=1)
+cv.imshow('Eroded', eroded)
+
+# Resize
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
 
 
 cv.waitKey(0)
